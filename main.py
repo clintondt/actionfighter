abb5bc1d35fecf6bd2cf9deedd71de5382ae459e
clintondt <clintondt@dulwich.org.uk>
2025-02-24 10:12:39 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+# import
+
+import pygame
+import math
+import random
+import time
+
+# color definitions
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
+
+# variable definitions
+
+# class definitions
+
+# function definitions
+
+# file definitions
+
+# screen definition
+
+screen_width = 
+
+pygame.init()
+
+# main code
+
+while not done:
+
+
+pygame.quit()
